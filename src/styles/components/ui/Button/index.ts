fc84a05bf85eccb7f4d0d3e8bcedf3786972c17b
Manas Salunke <123manassalunke@gmail.com>
2024-10-02 17:20:43 +0530

    chore: add gapX gapY

--- src/styles/components/ui/Button/index.ts
+++
@@ -1,2 +0,0 @@
-import { Button } from "../../base/button";
-export default Button;
--- src/styles/components/ui/Stack/Stack.tsx
+++ src/styles/components/ui/Stack/Stack.tsx
@@ -1,20 +1,31 @@
-import { ReactNode } from "react";
+import { PropsWithChildren } from "react";
 
 type StackProps = {
-  children: ReactNode;
   className?: string;
   direction?: "row" | "column";
-  gap?: string;
+  gapX?: "xxs" | "xs" | "sm" | "md" | "lg";
+  gapY?: "xxs" | "xs" | "sm" | "md" | "lg";
+  gap?: "xxs" | "xs" | "sm" | "md" | "lg";
 };
 
-function Stack(props: StackProps) {
+const GapPropsMap: Record<string, string> = {
+  default: "gap-0",
+  xxs: "gap-1",
+  xs: "gap-2",
+  sm: "gap-3",
+  md: "gap-4",
+  lg: "gap-6",
+};
+
+function Stack(props: PropsWithChildren<StackProps>) {
+  const gapclass = GapPropsMap[props.gap ?? "default"];
   return (
     <div
-      className={`flex flex-${props.direction} ${props.className} ${props.gap}`}
+      className={`flex flex-${props.direction} ${props.className} ${gapclass}`}
     >
       {props.children}
     </div>
   );
-}
+}  
 
 export default Stack;
