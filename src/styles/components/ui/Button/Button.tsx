124200939676aaafd131a9d9bf83dbde72e230dd
Manas Salunke <123manassalunke@gmail.com>
2024-10-03 21:32:27 +0530

    feat: add myButtonProps

--- src/styles/components/ui/Button/Button.tsx
+++ src/styles/components/ui/Button/Button.tsx
@@ -1,6 +1,11 @@
 import { PropsWithChildren } from "react";
 import { Button as BaseButton, ButtonProps } from "../../base/button";
 
-export default function Button(props: PropsWithChildren<ButtonProps>) {
+type MyButtonProps = Pick<
+  ButtonProps,
+  "onClick" | "disabled" | "variant" | "size"
+>;
+
+export default function Button(props: PropsWithChildren<MyButtonProps>) {
   return <BaseButton>{props.children}</BaseButton>;
 }
