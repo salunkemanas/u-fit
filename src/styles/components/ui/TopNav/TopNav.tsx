f901f256a81842621a54045fc7e7c574f125f60a
Manas Salunke <123manassalunke@gmail.com>
2024-10-13 22:11:50 +0530

    feat: add fixed top, side nav

--- src/styles/components/ui/TopNav/TopNav.tsx
+++ src/styles/components/ui/TopNav/TopNav.tsx
@@ -1,11 +1,22 @@
+import { Avatar, AvatarFallback, AvatarImage } from "../../base/avatar";
+import { Input } from "../../base/input";
+
 import Box from "../Box";
 import Stack from "../Stack";
+import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
 
 export default function TopNav() {
   return (
-    <Stack>
-      <Box>Logo |</Box>
-      <Box>Search bar</Box>
+    <Stack className="justify-between p-3 items-center px-6 bg-white w-screen">
+      <Box className="font-bold text-lg">Logo</Box>
+      <Stack className="w-1/3 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-300 items-center">
+        <Input placeholder="Search" className="" />
+        <MagnifyingGlassIcon className="ml-2" />
+      </Stack>
+      <Avatar className="w-10 h-10 rounded-full hover:shadow-md transition-shadow">
+        <AvatarImage src="https://github.com/shadcn.png" />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
     </Stack>
   );
 }
