543436555178d4ff0acea07087ba13ea3696a2dc
Manas Salunke <123manassalunke@gmail.com>
2024-10-15 20:45:16 +0530

    fix: add overflow-hidden insted of fixed

--- src/styles/components/ui/TopNav/TopNav.tsx
+++ src/styles/components/ui/TopNav/TopNav.tsx
@@ -1,21 +1,31 @@
-import { Avatar, AvatarFallback, AvatarImage } from "../../base/avatar";
-import { Input } from "../../base/input";
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@/styles/components/base/avatar";
+import { Input } from "@/styles/components/base/input";
 
-import Box from "../Box";
-import Stack from "../Stack";
+import Box from "@/styles/components/ui/Box";
+import Stack from "@/styles/components/ui/Stack";
 import { MagnifyingGlassIcon } from "@radix-ui/react-icons";
+import Typography from "@/styles/components/ui/Typography";
+import Image from "next/image";
 
 export default function TopNav() {
   return (
     <Stack className="justify-between p-3 items-center px-6 bg-white w-screen">
-      <Box className="font-bold text-lg">Logo</Box>
+      <Box className="font-bold text-lg">
+        <Image src="/logo.svg" alt="Logo" width={45} height={45} />
+      </Box>
       <Stack className="w-1/3 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-300 items-center">
         <Input placeholder="Search" className="" />
-        <MagnifyingGlassIcon className="ml-2" />
+        <MagnifyingGlassIcon className="size-7" />
       </Stack>
-      <Avatar className="w-10 h-10 rounded-full hover:shadow-md transition-shadow">
+      <Avatar className="size-10 rounded-full hover:shadow-md transition-shadow">
         <AvatarImage src="https://github.com/shadcn.png" />
-        <AvatarFallback>CN</AvatarFallback>
+        <AvatarFallback>
+          <Typography variant="large">CN</Typography>
+        </AvatarFallback>
       </Avatar>
     </Stack>
   );
