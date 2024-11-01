8847b8acbdd907d8776917d5cc2468e0255be0d0
Hatim Shakir <hatimshakir9179@gmail.com>
2024-11-01 14:43:00 +0530

    chore: next.config init

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: false,
+  },
+
+  // Enable React Strict Mode
+  reactStrictMode: true,
+
+  // Disable trailing slash
+  trailingSlash: false,
+
+  // Todo: add redirects here
+  async redirects() {
+    return [];
+  },
+};
 
 export default nextConfig;
